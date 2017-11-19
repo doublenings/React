8694080387a18bcf2499c7cfcbcb70aad0b337f4
Tamonwan <ningka_2540@hotmail.com>
2017-11-19 15:57:57 +0700

    first

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './pages/Login'
+import registerServiceWorker from './registerServiceWorker'
+import { BrowserRouter } from 'react-router-dom'
+import Routes from './routes'
+
+import './style/semantic.min.css'
+
+ReactDOM.render(
+<BrowserRouter>
+<Routes />
+</BrowserRouter>,
+document.getElementById('root')
+)
+registerServiceWorker()
